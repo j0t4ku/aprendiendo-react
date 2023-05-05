e7cffd1b38ccc37957b46de43ee7ded69ba93216
j0t4ku <j.ismar00@gmail.com>
2023-05-05 10:39:59 -0400

    Linter

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,88 +1,79 @@
-import { useState } from "react"
-import {Square} from './components/Square'
-import {TURNS,WINNER_COMBOS} from './constants';
-import { checkEndGame, checkWinner } from "./logic/board";
-import { WinnerModal } from "./components/WinnerModal";
-import { resetGameStorage, saveGameStorage } from "./logic/storage";
+import { useState } from 'react'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkEndGame, checkWinner } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+import { resetGameStorage, saveGameStorage } from './logic/storage'
 
-
-
-function App() {
-  const [board, setBoard]= useState(()=>{
-    const boardFromStorage= window.localStorage.getItem('board')
-    return boardFromStorage ? JSON.parse(boardFromStorage): Array(9).fill(null)
+function App () {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
   })
-  const [turn, setTurn]= useState(()=>{
-    const turnFromStorage= window.localStorage.getItem('turn')
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
     return turnFromStorage ?? TURNS.x
   })
-  const [winner, setWinner] = useState(null);   
+  const [winner, setWinner] = useState(null)
 
-
-  const resetGame=()=>{
+  const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.x)
     setWinner(null)
     resetGameStorage()
   }
 
-  
-
-  const updateBoard= (index)=>{
-    //no actualizar si ya tiene algo
-    if(board[index] || winner) return
-    //actualizar el tablero
-    const newBoard= [...board]
-    newBoard[index]= turn
+  const updateBoard = (index) => {
+    // no actualizar si ya tiene algo
+    if (board[index] || winner) return
+    // actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
     setBoard(newBoard)
 
-    //cambiar turnos
+    // cambiar turnos
     const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
     setTurn(newTurn)
-    //Guardar Partida
-    
-    saveGameStorage({board: newBoard, turn: newTurn})
+    // Guardar Partida
 
+    saveGameStorage({ board: newBoard, turn: newTurn })
 
-    //verificar ganador
-    const newWinner= checkWinner(newBoard)
-    if (newWinner){
+    // verificar ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
       setWinner(newWinner)
-    } else if(checkEndGame(newBoard)){
+    } else if (checkEndGame(newBoard)) {
       setWinner(false)
     }
   }
 
-
-  
-
   return (
-   <main className="board">
-    <h1>Tic Tac Toe</h1>
-    <button onClick={resetGame}>Reiniciar el Juego</button>
-    <section className="game">
-      {
-        board.map((square, index)=>{
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reiniciar el Juego</button>
+      <section className='game'>
+        {
+        board.map((square, index) => {
           return (
-            <Square 
-            key={index} 
-            index={index}
-            updateBoard={updateBoard}
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
             >
               {square}
             </Square>
           )
         })
       }
-    </section>
-    <section className="turn">
-      <Square isSelected={turn === TURNS.x}>{TURNS.x}</Square>
-      <Square isSelected={turn === TURNS.o}>{TURNS.o}</Square>
-    </section>
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.x}>{TURNS.x}</Square>
+        <Square isSelected={turn === TURNS.o}>{TURNS.o}</Square>
+      </section>
 
-      <WinnerModal resetGame={resetGame} winner={winner}/>
+      <WinnerModal resetGame={resetGame} winner={winner} />
 
-   </main>
+    </main>
   )
 }
 
